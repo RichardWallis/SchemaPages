0d2e4565f6dc87983867110a01eee9ce8758f8e1
Dataliberate <rjw@dataliberate.com>
2020-07-28 15:26:22 +0100

    Tweaks

--- sdopageframe.py
+++ sdopageframe.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python2.7
 import rdflib
-from apirdfterm import *
+from sdotermsource import *
+from sdoterm import *
 
 
 triplesfile = "schemaorg-all-https.nt"
 termgraph = rdflib.Graph()
 termgraph.parse(triplesfile, format="nt")
 termgraph.bind("schema","https://schema.org/")
+termgraph.bind("owl","http://www.w3.org/2002/07/owl#")
+termgraph.bind("dc","http://purl.org/dc/elements/1.1/")
+termgraph.bind("dct","http://purl.org/dc/terms/")
 
 print ("loaded %s triples" % len(termgraph))
 
-VTerm.setQueryGraph(termgraph)
+SdoTermSource.setQueryGraph(termgraph)
 
-term = VTerm.getTerm("FoodEstablishment")
+term = SdoTermSource.getTerm("FoodEstablishment")
 
-print()
-print("TYPE: %s" % term.ttype)
+print("")
+print("TYPE: %s" % term.termType)
 print("URI: %s" % term.uri)
 print("ID: %s" % term.id)
 print("LABEL: %s" % term.label)
-print("PARENT: %s" % term.parent)
-print("PROPS: %s" % term.props)
+
 print("COMMENT: %s" % term.comment)
-print("TERMSSTACK: %s" % term.termStack)
+print("COMMENTS: %s" % term.comments)
+print("EQIVALENTS: %s" % term.equivalents)
+print("EXAMPLES: %s" % term.examples)
+print("PENDING: %s" % term.pending)
+print("RETIRED: %s" % term.retired)
+print("SOURCES: %s" % term.sources)
+print("SUBS: %s" % term.subs)
+print("SUPERS: %s" % term.supers)
+print("SUPERSEDEDBY: %s" % term.supersededBy)
+print("SUPERSEDES: %s" % term.supersedes)
+print("TERMSTACK: %s" % term.termStack)
 
